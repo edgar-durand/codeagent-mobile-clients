79ea496497fab842be9591c12373cc0cf2e9809f
Edgar Durand <edgar@privacyhawk.com>
2026-05-17 18:27:40 -0300

    fix(jetbrains-plugin): import terminalopsservice in controllertoolwindowfactory

--- apps/jetbrains-plugin/src/main/kotlin/com/windsurf/controller/ui/ControllerToolWindowFactory.kt
+++ apps/jetbrains-plugin/src/main/kotlin/com/windsurf/controller/ui/ControllerToolWindowFactory.kt
@@ -20,6 +20,7 @@ import com.windsurf.controller.services.IdeIntegrationService
 import com.windsurf.controller.services.PairingService
 import com.windsurf.controller.services.SettingsService
 import com.windsurf.controller.services.TerminalAgentService
+import com.windsurf.controller.services.TerminalOpsService
 import com.windsurf.controller.services.McpConfigWriterService
 import com.windsurf.controller.services.McpConfigureRequest
 import com.windsurf.controller.services.McpEntry
